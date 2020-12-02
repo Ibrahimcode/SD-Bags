dd815ca7ed6cdd3d3574c17460173c317f7a13b2
DwainDavies <60291142+DwainDavies@users.noreply.github.com>
2020-12-02 19:15:59 +0200

    latest information

---
+++ app/src/main/java/com/example/sdbags/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sdbags;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    primplementation 'com.google.android.gms:play-services-auth:19.0.0'otected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Shop.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
