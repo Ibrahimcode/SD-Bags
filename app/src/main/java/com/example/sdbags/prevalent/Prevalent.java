eed92a3efe255e611d21f63e1345d8fa7772f22a
DwainDavies <60291142+DwainDavies@users.noreply.github.com>
2020-12-11 03:33:38 +0200

    Most recent updates

---
+++ app/src/main/java/com/example/sdbags/prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.sdbags.prevalent;
+
+import com.example.sdbags.model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
